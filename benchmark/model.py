998af24e9c70eb8f09d5f77714578cdcca097b39
Yuriy Tyshetskiy <yuriy.tyshetskiy@data61.csiro.au>
2017-01-09 12:19:48 +1100

    Adding neural net models

--- benchmark/model.py
+++ benchmark/model.py
@@ -5,7 +5,7 @@ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 Abstract model for semantic labelling/typing.
 Evaluation will be based on this abstract model.
 """
-
+from neural_nets import CNN, Column_Labeler
 
 class AbstractModel(object):
     def __init__(self):
@@ -70,19 +70,30 @@ class KarmaDSLModel(AbstractModel):
         pass
 
 class NNetModel(AbstractModel):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        assert "model_type" in args
+
+        self.column_labeler = Column_Labeler('cnn@charseq')
+
+        if isinstance(args["model_type"], list):
+            # ensemble
+            self.model = ensemble()
+        else:
+            if args['model_type'] == "CNN":
+                self.model = self.column_labeler.classifier
+                self.description = "CNN with "
+
 
     def reset(self):
         pass
 
-    def define_training_data(self):
-        pass
+    def define_training_data(self, train_files, train_labels):
+        self.column_labeler = Column_Labeler([self.model], train_files, train_labels)
 
     def train(self):
         pass
 
-    def predict(self):
+    def predict(self, test_file):
         pass
 
     def evaluate(self):
