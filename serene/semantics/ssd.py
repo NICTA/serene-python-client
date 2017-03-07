4decc42a175c247ca4b2426697663563ff5f1faa
Alex Collins <alex.collins@nicta.com.au>
2017-03-07 23:19:27 +1100

    updating ssd code

--- serene/semantics/ssd.py
+++ serene/semantics/ssd.py
@@ -4,12 +4,13 @@ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 Code for the SSD dataset description object
 """
+import json
 import logging
 import random
 import time
-import pandas as pd
+from collections import OrderedDict
 
-from serene.ssd_util import SSDJsonBuilder
+import pandas as pd
 from serene.elements import Transform, Mapping, \
     Column, ClassNode, DataNode, Link, \
     TransformList, IdentTransform
@@ -19,8 +20,6 @@ from .base import BaseSemantic
 _logger = logging.getLogger()
 _logger.setLevel(logging.DEBUG)
 
-#LINK_NAME = "relationship"
-#DATA_NODE_LINK_NAME = "property"
 
 class SSD(object):
     """
@@ -497,3 +496,97 @@ class SSD(object):
         full_str = '\n\t'.join(items)
 
         return "[\n\t{}\n]".format(full_str)
+
+
+class SSDJsonBuilder(object):
+    """
+    Helper class to build up the json output for the SSD file.
+    """
+    def __init__(self, ssd):
+        """
+
+        :param ssd:
+        """
+        self._ssd = ssd
+
+        self._all_nodes = [n for n in
+                           self._ssd.class_nodes + self._ssd.data_nodes
+                           if n is not None]
+
+        self._node_map = {m: i for i, m in enumerate(self._all_nodes)}
+        self._attr_map = {m: i for i, m in enumerate(self._ssd.mappings)}
+
+    def to_dict(self):
+        """
+
+        :return:
+        """
+        d = OrderedDict()
+        d["version"] = self._ssd.version
+        d["name"] = self._ssd.file
+        d["columns"] = self.columns
+        d["attributes"] = self.attributes
+        d["ontology"] = [o.set_filename for o in self._ssd.ontologies]
+        d["semanticModel"] = self.semantic_model
+        d["mappings"] = self.mappings
+
+        return d
+
+    def to_json(self):
+        """
+
+        :return:
+        """
+        return json.dumps(self.to_dict(), indent=4)
+
+    @property
+    def columns(self):
+        """
+
+        :return:
+        """
+        return [
+            {
+                "id": c.index,
+                "name": c.name
+            } for c in self._ssd.columns]
+
+    @property
+    def attributes(self):
+        """
+
+        :return:
+        """
+        return [
+            {
+                "id": self._attr_map[m],
+                "name": m.column.name,
+                "label": m.transform.name,
+                "columnIds": [m.column.index],
+                "sql": m.transform.apply(m.column)
+            } for m in self._ssd.mappings]
+
+    @property
+    def semantic_model(self):
+        """
+
+        :return:
+        """
+        return {
+            "nodes": [node.ssd_output(index)
+                      for index, node in enumerate(self._all_nodes)],
+            "links": [link.ssd_output(self._node_map)
+                      for link in self._ssd.links]
+        }
+
+    @property
+    def mappings(self):
+        """
+
+        :return:
+        """
+        return [
+            {
+                "attribute": self._attr_map[m],
+                "node": self._node_map[m.node]
+            } for m in self._ssd.mappings if m.node is not None]
