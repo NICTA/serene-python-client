824201d6b1290a1b337e6d4fe640b1ad0b79ad8e
Chao Li <chao.li@data61.csiro.au>
2017-04-03 11:36:53 +1000

    Test Octopus API

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -8,6 +8,7 @@ from unittest2 import TestCase
 from serene.api.data_api import DataSetAPI
 from serene.api.exceptions import InternalError
 from serene.api.model_api import ModelAPI
+from serene.api.octopus_api import OctopusAPI
 from serene.api.ontology_api import OntologyAPI, OwlFormat
 from serene.api.ssd_api import SsdAPI
 from serene.elements import DataSet, Ontology, SSD
@@ -153,8 +154,8 @@ class TestModelAPI(TestCase):
         }
         self.cost_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         self.resampling_strategy = "ResampleToMean"
-        self.numBags = 60
-        self.bagSize = 90
+        self.num_bags = 60
+        self.bag_size = 90
 
         self.data = {
             "features": self.feature_config,
@@ -164,8 +165,8 @@ class TestModelAPI(TestCase):
             "labelData": self.labels,
             "costMatrix": self.cost_matrix,
             "resamplingStrategy": self.resampling_strategy,
-            "numBags": self.numBags,
-            "bagSize": self.bagSize
+            "numBags": self.num_bags,
+            "bagSize": self.bag_size
         }
 
     def test_post(self):
@@ -181,8 +182,8 @@ class TestModelAPI(TestCase):
             labels=self.labels,
             cost_matrix=self.cost_matrix,
             resampling_strategy=self.resampling_strategy,
-            numBags=self.numBags,
-            bagSize=self.bagSize)
+            num_bags=self.num_bags,
+            bag_size=self.bag_size)
 
         self.assertEqual(result, message)
         self.connection.post.assert_called_with(self.uri, json=self.data)
@@ -198,7 +199,7 @@ class TestModelAPI(TestCase):
             labels=self.labels,
             cost_matrix=self.cost_matrix,
             resampling_strategy=self.resampling_strategy,
-            numBags=self.numBags)
+            num_bags=self.num_bags)
 
         self.assertRaises(InternalError, api_post)
 
@@ -217,8 +218,8 @@ class TestModelAPI(TestCase):
             labels=self.labels,
             cost_matrix=self.cost_matrix,
             resampling_strategy=self.resampling_strategy,
-            numBags=self.numBags,
-            bagSize=self.bagSize)
+            num_bags=self.num_bags,
+            bag_size=self.bag_size)
 
         self.assertEqual(result, message)
         self.connection.post.assert_called_with(
@@ -237,7 +238,7 @@ class TestModelAPI(TestCase):
             labels=self.labels,
             cost_matrix=self.cost_matrix,
             resampling_strategy=self.resampling_strategy,
-            numBags=self.numBags)
+            num_bags=self.num_bags)
 
         self.assertRaises(InternalError, api_update)
 
@@ -490,6 +491,52 @@ class TestOntologyAPI(TestCase):
         self.assertRaises(InternalError, api_owl_file)
 
 
+def init_test_ssd(target):
+    target.dataset_json = {
+        "dateCreated": "2017-03-16T15:29:03.388",
+        "dateModified": "2017-03-16T15:29:03.388",
+        "description": "",
+        "filename": "businessInfo.csv",
+        "id": 2035625835,
+        "path": "/Users/li151/Dev/serene/./storage/datasets/2035625835/businessinfo.csv",
+        "typeMap": {},
+        "columns": [
+            {
+                "datasetID": 2035625835,
+                "id": 1246005714,
+                "index": 0,
+                "logicalType": "string",
+                "name": "company",
+                "path": "/Users/li151/Dev/serene/./storage/datasets/2035625835/businessinfo.csv",
+                "sample": ["Data61"],
+                "size": 59
+            },
+            {
+                "datasetID": 2035625835,
+                "id": 281689915,
+                "index": 1,
+                "logicalType": "string",
+                "name": "ceo",
+                "path": "/Users/li151/Dev/serene/./storage/datasets/2035625835/businessinfo.csv",
+                "sample": ["Garv Mcowen"],
+                "size": 59
+            }
+        ]
+    }
+
+    target.dataset = DataSet(target.dataset_json)
+    target.ontology = Ontology().update({
+        "name": __file__,
+        "id": 123,
+        "description": "test ontology",
+        "dateCreated": "2017-03-16T15:29:03.388",
+        "dateModified": "2017-03-16T15:29:03.388"
+    })
+
+    target.ssd = SSD(target.dataset, target.ontology, "test ssd")
+    target.ssd_json = target.ssd.json
+
+
 class TestSsdAPI(TestCase):
     def setUp(self):
         self.connection = Mock(Session())
@@ -499,47 +546,7 @@ class TestSsdAPI(TestCase):
         self.api = SsdAPI(self.root_uri, self.connection)
         self.response = Mock(Response())
 
-        self.dataset_json = {
-            "dateCreated": "2017-03-16T15:29:03.388",
-            "dateModified": "2017-03-16T15:29:03.388",
-            "description": "",
-            "filename": "businessInfo.csv",
-            "id": 2035625835,
-            "path": "/Users/li151/Dev/serene/./storage/datasets/2035625835/businessinfo.csv",
-            "typeMap": {},
-            "columns": [
-                {
-                    "datasetID": 2035625835,
-                    "id": 1246005714,
-                    "index": 0,
-                    "logicalType": "string",
-                    "name": "company",
-                    "path": "/Users/li151/Dev/serene/./storage/datasets/2035625835/businessinfo.csv",
-                    "sample": ["Data61"],
-                    "size": 59
-                },
-                {
-                    "datasetID": 2035625835,
-                    "id": 281689915,
-                    "index": 1,
-                    "logicalType": "string",
-                    "name": "ceo",
-                    "path": "/Users/li151/Dev/serene/./storage/datasets/2035625835/businessinfo.csv",
-                    "sample": ["Garv Mcowen"],
-                    "size": 59
-                }
-            ]
-        }
-
-        self.dataset = DataSet(self.dataset_json)
-        self.ontology = Ontology().update({
-            "name": __file__,
-            "id": 123,
-            "description": "test ontology",
-            "dateCreated": "2017-03-16T15:29:03.388",
-            "dateModified": "2017-03-16T15:29:03.388"
-        })
-        self.ssd_json = SSD(self.dataset, self.ontology, "test ssd").json
+        init_test_ssd(self)
 
     def test_keys(self):
         keys = [1, 2]
@@ -612,6 +619,155 @@ class TestSsdAPI(TestCase):
 
         self.assertRaises(InternalError, api_item)
 
+    def test_delete(self):
+        message = "Deleted"
+        key = 1
+        self.response.status_code = 200
+        self.response.json = Mock(return_value=message)
+        self.connection.delete = Mock(return_value=self.response)
+        result = self.api.delete(key)
+
+        self.assertEqual(result, message)
+        self.connection.delete.assert_called_with(self.uri + str(key))
+
+    def test_delete_with_connection_exception(self):
+        self.connection.delete = Mock(side_effect=Exception)
+        api_delete = partial(self.api.delete, 1)
+
+        self.assertRaises(InternalError, api_delete)
+
+
+class TestOctopusAPI(TestCase):
+    def setUp(self):
+        self.connection = Mock(Session())
+        self.root_uri = "http://localhost/"
+        self.octopus_path = "octopus/"
+        self.uri = self.root_uri + self.octopus_path
+        self.api = OctopusAPI(self.root_uri, self.connection)
+        self.response = Mock(Response())
+
+        init_test_ssd(self)
+
+        self.name = "test octopus"
+        self.feature_config = {
+            "activeFeatures": [
+                "num-unique-vals"
+            ]
+        }
+        self.description = "test model"
+        self.model_type = "randomForest"
+        self.resampling_strategy = "ResampleToMean"
+        self.num_bags = 60
+        self.bag_size = 90
+        self.modeling_props = {"topkSteinerTrees": 1}
+
+        self.data = {
+            "ssds": [self.ssd],
+            "name": self.name,
+            "description": self.description,
+            "modelType": self.model_type,
+            "resamplingStrategy": self.resampling_strategy,
+            "features": self.feature_config,
+            "numBags": self.num_bags,
+            "bagSize": self.bag_size,
+            "ontologies": [self.ontology],
+            "modelingProps": self.modeling_props
+        }
+
+    def test_post(self):
+        message = "Created"
+        self.response.status_code = 200
+        self.response.json = Mock(return_value=message)
+        self.connection.post = Mock(return_value=self.response)
+        result = self.api.post(
+            ssds=[self.ssd],
+            name=self.name,
+            description=self.description,
+            feature_config=self.feature_config,
+            model_type=self.model_type,
+            resampling_strategy=self.resampling_strategy,
+            num_bags=self.num_bags,
+            bag_size=self.bag_size,
+            ontologies=[self.ontology],
+            modeling_props=self.modeling_props)
+
+        self.assertEqual(result, message)
+        self.connection.post.assert_called_with(self.uri, json=self.data)
+
+    def test_post_with_connection_exception(self):
+        self.connection.post = Mock(side_effect=Exception)
+        api_post = partial(
+            self.api.post,
+            ssds=[self.ssd],
+            name=self.name,
+            description=self.description,
+            feature_config=self.feature_config,
+            model_type=self.model_type,
+            resampling_strategy=self.resampling_strategy,
+            num_bags=self.num_bags,
+            bag_size=self.bag_size,
+            ontologies=[self.ontology],
+            modeling_props=self.modeling_props)
+
+        self.assertRaises(InternalError, api_post)
+
+    def test_update(self):
+        message = "Updated"
+        key = 1
+        self.response.status_code = 200
+        self.response.json = Mock(return_value=message)
+        self.connection.post = Mock(return_value=self.response)
+        result = self.api.update(
+            key,
+            ssds=[self.ssd],
+            name=self.name,
+            description=self.description,
+            feature_config=self.feature_config,
+            model_type=self.model_type,
+            resampling_strategy=self.resampling_strategy,
+            num_bags=self.num_bags,
+            bag_size=self.bag_size,
+            ontologies=[self.ontology],
+            modeling_props=self.modeling_props)
+
+        self.assertEqual(result, message)
+        self.connection.post.assert_called_with(
+            self.uri + str(key),
+            json=self.data)
+
+    def test_update_with_connection_exception(self):
+        self.connection.post = Mock(side_effect=Exception)
+        api_update = partial(
+            self.api.update,
+            1,
+            ssds=[self.ssd],
+            name=self.name,
+            description=self.description,
+            feature_config=self.feature_config,
+            model_type=self.model_type,
+            resampling_strategy=self.resampling_strategy,
+            num_bags=self.num_bags,
+            bag_size=self.bag_size,
+            ontologies=[self.ontology],
+            modeling_props=self.modeling_props)
+
+        self.assertRaises(InternalError, api_update)
+
+    def test_item(self):
+        key = 1
+        self.response.status_code = 200
+        self.response.json = Mock(return_value=self.data)
+        self.connection.get = Mock(return_value=self.response)
+        result = self.api.item(key)
+
+        self.assertEqual(result, self.data)
+        self.connection.get.assert_called_with(self.uri + str(key))
+
+    def test_item_with_connection_exception(self):
+        self.connection.get = Mock(side_effect=Exception)
+        api_item = partial(self.api.item, 1)
+
+        self.assertRaises(InternalError, api_item)
 
     def test_delete(self):
         message = "Deleted"
@@ -629,3 +785,61 @@ class TestSsdAPI(TestCase):
         api_delete = partial(self.api.delete, 1)
 
         self.assertRaises(InternalError, api_delete)
+
+    def test_keys(self):
+        keys = [1, 2]
+        self.response.status_code = 200
+        self.response.json = Mock(return_value=keys)
+        self.connection.get = Mock(return_value=self.response)
+        result = self.api.keys()
+
+        self.assertEqual(result, keys)
+        self.connection.get.assert_called_with(self.uri)
+
+    def test_keys_with_connection_exception(self):
+        self.connection.get = Mock(side_effect=Exception)
+
+        self.assertRaises(InternalError, self.api.keys)
+
+    def test_train(self):
+        key = 1
+        self.response.status_code = 200
+        self.connection.post = Mock(return_value=self.response)
+        result = self.api.train(key)
+
+        self.assertEqual(result, True)
+        self.connection.post.assert_called_with(
+            self.uri + str(key) + "/train")
+
+    def test_train_with_connection_exception(self):
+        self.connection.post = Mock(side_effect=Exception)
+
+        self.assertRaises(InternalError, partial(self.api.train, 1))
+
+    def test_predict(self):
+        octopusKey = 1
+        dataSetKey = 2
+        predictions = {
+            "predictions": [{
+                "ssd": {
+                    "name": "test ssd",
+                },
+                "score": {
+                    "linkCost": 0.5
+                }
+            }]
+        }
+
+        self.response.status_code = 200
+        self.response.json = Mock(return_value=predictions)
+        self.connection.post = Mock(return_value=self.response)
+        result = self.api.predict(octopusKey, dataSetKey)
+
+        self.assertEqual(result, predictions)
+        self.connection.post.assert_called_with(
+            self.uri + str(octopusKey) + "/predict/" + str(dataSetKey))
+
+    def test_predict_with_connection_exception(self):
+        self.connection.post = Mock(side_effect=Exception)
+
+        self.assertRaises(InternalError, partial(self.api.predict, 1, 2))
